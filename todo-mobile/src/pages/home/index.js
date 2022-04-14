49b56663700312faed6e311833409af967110f4c
Valdeci97 <moura97.valdeci@gmail.com>
2022-04-14 14:17:25 -0300

    New: Create filter options

--- todo-mobile/src/pages/home/index.js
+++ todo-mobile/src/pages/home/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Text } from 'react-native';
 
 import style from './styles';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
 
+const options = ['Todos', 'Hoje', 'Semana', 'Mês', 'Ano'];
+
 export default function Home() {
+  const [isActive, setIsActive] = useState('Todos');
+
   return (
     <View style={ style.container }>
       <Header showNotification={ true } showBack={ false } />
+      <View style={ style.filter }>
+        {
+          options.map((option) =>
+            <TouchableOpacity onPress={() => setIsActive(option)}>
+            <Text style={ isActive === option ? style.filterActive : style.filterInactive }>
+                { option }
+              </Text>
+            </TouchableOpacity>
+          )
+        }
+      </View>
       <Footer showSave={ 'add' } />
     </View>
   );
--- todo-mobile/src/pages/home/styles.js
+++ todo-mobile/src/pages/home/styles.js
@@ -6,6 +6,24 @@ const style = StyleSheet.create({
     backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'flex-start',
+  },
+  filter: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    height: 50,
+    justifyContent: 'space-evenly',
+    width: '100%',
+  },
+  filterActive: {
+    color: '#000',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  filterInactive: {
+    color: '#99AAEA',
+    fontSize: 16,
+    fontWeight: 'bold',
+    opacity: 0.4
   }
 });
 
